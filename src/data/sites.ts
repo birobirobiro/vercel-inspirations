fc7e57269a6aa098f7fc2d68c1abd941069a6828
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-05-04 15:06:50 -0300

    initial commit

---
+++ src/data/sites.ts
@@ -0,0 +1,52 @@
+const sites = [
+  {
+    name: "Raycast",
+    image: "/sites/Raycast.png",
+    link: "https://raycast.com/",
+    colors: ["#ffafcc", "#219ebc"],
+  },
+  {
+    name: "Linear",
+    image: "/sites/Linear.png",
+    link: "https://linear.app/",
+    colors: ["#e63946", "#606c38"],
+  },
+  {
+    name: "Linear",
+    image: "/sites/Linear.png",
+    link: "https://linear.app/",
+    colors: ["#e63946", "#606c38"],
+  },
+  {
+    name: "Linear",
+    image: "/sites/Linear.png",
+    link: "https://linear.app/",
+    colors: ["#e63946", "#606c38"],
+  },
+  {
+    name: "Linear",
+    image: "/sites/Linear.png",
+    link: "https://linear.app/",
+    colors: ["#e63946", "#606c38"],
+  },
+  {
+    name: "Linear",
+    image: "/sites/Linear.png",
+    link: "https://linear.app/",
+    colors: ["#e63946", "#606c38"],
+  },
+  {
+    name: "Linear",
+    image: "/sites/Linear.png",
+    link: "https://linear.app/",
+    colors: ["#e63946", "#606c38"],
+  },
+  {
+    name: "Linear",
+    image: "/sites/Linear.png",
+    link: "https://linear.app/",
+    colors: ["#e63946", "#606c38"],
+  },
+];
+
+export default sites;
